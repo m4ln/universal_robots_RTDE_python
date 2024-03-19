aa707a5fc9da95a424b1bbc52e6a39c56352ff82
m4ln <runzmarlen@gmail.com>
2024-03-19 14:15:49 +0800

    code docu

--- examples/example_control_loop.py
+++ examples/example_control_loop.py
@@ -33,20 +33,27 @@ import rtde.rtde_config as rtde_config
 
 # logging.basicConfig(level=logging.INFO)
 
-ROBOT_HOST = "localhost"
+# Define the robot's host IP and port
+ROBOT_HOST = "169.254.106.113"
 ROBOT_PORT = 30004
+# Define the configuration file name
 config_filename = "control_loop_configuration.xml"
 
+# Flag to keep the control loop running
 keep_running = True
 
 logging.getLogger().setLevel(logging.INFO)
 
+# Parse the configuration file
 conf = rtde_config.ConfigFile(config_filename)
+# Get the recipes for state, setpoint, and watchdog from the configuration file
 state_names, state_types = conf.get_recipe("state")
 setp_names, setp_types = conf.get_recipe("setp")
 watchdog_names, watchdog_types = conf.get_recipe("watchdog")
 
+# Create a connection to the robot
 con = rtde.RTDE(ROBOT_HOST, ROBOT_PORT)
+# Establish the connection
 con.connect()
 
 # get controller version
@@ -57,10 +64,11 @@ con.send_output_setup(state_names, state_types)
 setp = con.send_input_setup(setp_names, setp_types)
 watchdog = con.send_input_setup(watchdog_names, watchdog_types)
 
-# Setpoints to move the robot to
+# Setpoints to move the robot between these two poses (in joint space)
 setp1 = [-0.12, -0.43, 0.14, 0, 3.11, 0.04]
 setp2 = [-0.12, -0.51, 0.21, 0, 3.11, 0.04]
 
+# Initialize the setpoint registers to 0
 setp.input_double_register_0 = 0
 setp.input_double_register_1 = 0
 setp.input_double_register_2 = 0
@@ -73,6 +81,7 @@ watchdog.input_int_register_0 = 0
 
 
 def setp_to_list(sp):
+    '''Convert the setpoint to a list.'''
     sp_list = []
     for i in range(0, 6):
         sp_list.append(sp.__dict__["input_double_register_%i" % i])
@@ -80,6 +89,7 @@ def setp_to_list(sp):
 
 
 def list_to_setp(sp, list):
+    '''Convert the list to a setpoint.'''
     for i in range(0, 6):
         sp.__dict__["input_double_register_%i" % i] = list[i]
     return sp
@@ -98,18 +108,23 @@ while keep_running:
     if state is None:
         break
 
-    # do something...
+    # If a move has been completed and the robot is ready for a new one
     if move_completed and state.output_int_register_0 == 1:
         move_completed = False
+        # Determine the new setpoint
         new_setp = setp1 if setp_to_list(setp) == setp2 else setp2
+        # Update the setpoint
         list_to_setp(setp, new_setp)
         print("New pose = " + str(new_setp))
         # send new setpoint
         con.send(setp)
+        # set watchdog
         watchdog.input_int_register_0 = 1
+    # If a move is in progress and the robot is not ready for a new one
     elif not move_completed and state.output_int_register_0 == 0:
         print("Move to confirmed pose = " + str(state.target_q))
         move_completed = True
+        # reset watchdog
         watchdog.input_int_register_0 = 0
 
     # kick watchdog
