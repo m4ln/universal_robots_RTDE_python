591942a724b56b96a831b8bf54c017d927e65b29
m4ln <runzmarlen@gmail.com>
2024-03-21 19:56:50 +0800

    config file to run the examples

---
+++ config.py
@@ -0,0 +1,11 @@
+''' This file is for the configuration of the robot and the control loop.'''
+
+# Define the robot's host IP and port
+ROBOT_HOST = "169.254.91.163"
+ROBOT_PORT = 30004
+
+# TCP positions (x,y,z,rx,ry,rz in base view) between which the robot moves
+tcp_pos_1 = [0.2, -0.2, 0.1, 0, 3.11, 0.04]
+tcp_pos_2 = [-0.2, -0.5, 0.1, 0, 3.11, 0.04]
+tcp_pos_3 = [-0.2, -0.2, 0.1, 0, 3.11, 0.04]
+tcp_pos_4 = [0.2, -0.5, 0.1, 0, 3.11, 0.04]
