4211ee67874103392a553de9b6a29019a4c4760f
m4ln <runzmarlen@gmail.com>
2024-04-16 19:30:34 +0800

    added test for playing 3 strings

--- config.py
+++ config.py
@@ -8,7 +8,34 @@ ROBOT_PORT = 30004
 EPSILON = 0.001
 
 # enter the configuration of the tcp points you want to use
-tcp_pos_config = [0,1]
+# tcp_pos_config = [0,1,0,2,3,4,3,2]
+# tcp_pos_config = [7,8,9,10,9,8]
+tcp_pos_config = [0, 1, 0, 2, 3, 4, 3, 4, 3, 5, 6, 7, 8, 9, 10, 9, 10, 8, 7, 6]
+
+# RX,RY,RZ for planar on x-y axis = 2.951, 1.077, ...
+
+# single string (move in y direction)
+tcp_pos_0 = [-0.24, 0.015, 0.119, 2.255, 2.221, 0] # moving back
+tcp_pos_1 = [-0.24, -0.266, 0.122, 2.255, 2.221, 0] # playing
+
+# lower string
+tcp_pos_2 = [-0.09, -0.212, 0.209, 0.49, 3.118, 0.035] # moving back
+tcp_pos_3 = [-0.09, -0.212, 0.199, 0.49, 3.118, 0.035] # position on string
+tcp_pos_4 = [0.288, -0.266, 0.219, 0.680, 3.144, 0.069] # moving front
+
+# upper string
+tcp_pos_5 = [-0.258, -0.212, 0.209, 0.49, 3.118, 0.035] # moving more back (from pos 3)
+tcp_pos_6 = [-0.258, -0.212, 0.209, 1.212, 2.908, 0.035] # rotating bow out of frame
+tcp_pos_7 = [-0.098, -0.136, 0.522, 1.431, 2.755, -0.091] # moving up
+tcp_pos_8 = [-0.011, -0.167, 0.522, 0.617, 3.028, -0.072] # moving to new position (string)
+tcp_pos_9 = [0.0393, -0.171, 0.406, 0.655, 2.799, -0.257] # position on string
+tcp_pos_10 = [0.23, -0.237, 0.475, 0.655, 2.799, -0.257] # moving front
+
+
+### test for interpolation
+# tcp_pos_0 = [-0.15, -0.160, 0.178, 2.147, 2.387, 0.076]
+# tcp_pos_1 = [-0.15, -0.400, 0.148, 2.147, 2.387, 0.076]
+# tcp_pos_2 = [-0.15, -0.160, 0.178, 0.395, 3.16, 0.108]
 
 ####
 # TCP positions (x,y,z,rx,ry,rz in base view) between which the robot moves
@@ -16,8 +43,8 @@ tcp_pos_config = [0,1]
 ####
 # playing the string with the bridge
 # the bow starts from behind and moves a little downwards while going a bit up (-4mm in z direction)
-tcp_pos_0 = [-0.15, -0.160, 0.152, 2.147, 2.387, 0.076]
-tcp_pos_1 = [-0.15, -0.400, 0.148, 2.147, 2.387, 0.076]
+# tcp_pos_0 = [-0.15, -0.160, 0.152, 2.147, 2.387, 0.076]
+# tcp_pos_1 = [-0.15, -0.400, 0.148, 2.147, 2.387, 0.076]
 
 # tcp_pos_0 = [-0.15, -0.251, 0.130, 2.147, 2.387, 0.076]
 # tcp_pos_1 = [-0.15, -0.166, 0.130, 2.147, 2.387, 0.076]
@@ -25,37 +52,10 @@ tcp_pos_1 = [-0.15, -0.400, 0.148, 2.147, 2.387, 0.076]
 # tcp_pos_0 = [-0.119, -0.528, 0.388, 0.03, -2.8, 1.5]
 # tcp_pos_1 = [-0.119, -0.528, 0.388, 0.13, -2.8, 1.5]
 
-tcp_pos_2 = [-0.104, -0.485, 0.213, 0.223, 3.136, -0.024]
-# tcp_pos_1 = [-0.104, -0.485, 0.213, 0.4, 3.136, -0.024]
-
 # daniel
 # tcp_pos_0 = [-0.12, -0.51, 0.21, 0, 3.11, 0.04]
 # tcp_pos_1 = [-0.12, -0.51, 0.21, 0.6, 3.11, 0.04]
 # tcp_pos_2 = [-0.12, -0.51, 0.21, 0, 3.11, 0.14]
-
-# tcp_pos_0 = [-0.084, -0.252, 0.121, 2.249, 2.117, 0.131]
-# tcp_pos_1 = tcp_pos_0.copy()
-# tcp_pos_1[0] += EPSILON
-# tcp_pos_2 = [-0.129, -0.303, 0.11, 2.24, 1.281, -0.095]
-# tcp_pos_3 = [-0.13, -0.324, 0.451, 1.3, 1.233, -1.209]
-
-# tcp_pos_0 = [0.245, -0.0973, 0.0959, 2.248, -2.189, 0.079]
-# tcp_pos_1 = [0.245, -0.4043, 0.0959, 2.248, -2.189, 0.079]
-# tcp_pos_2 = [0.081, -0.259, 0.1546, 1.265, -2.686, 0.008]
-tcp_pos_3 = [0.154, -0.2, 0.273, 1.974, -3.886, -1.485]
-tcp_pos_4 = [0.154, -0.2, 0.273, 1.974, -3.886, -1.49]
-# tcp_pos_4 = [-0.033, -0.251, 0.273, 0.359, -4.698, 0.233]
-tcp_pos_5 = [0.09643, -0.33, 0.532, 0.399, 2.318, -0.225]
-tcp_pos_6 = [-0.120, -0.315, 0.287, 0.331, 2.355, -0.093]
-tcp_pos_7 = [0.09643, -0.33, 0.532, 0.399, 2.32, -0.225]
-tcp_pos_8 = [0.154, -0.2, 0.273, 1.98, -3.886, -1.485] # tcp5
-tcp_pos_9 = [0.154, -0.2, 0.273, 1.974, -3.886, -1.49]#tcp4 [-0.61, -0.17, 0.97, 1.22, 1.14, 1.06]
-tcp_pos_10 = [0.081, -0.259, 0.1546, 1.265, -2.69, 0.008] # tcp3 [-0.33, -0.42, 0.24, 0.04, 0.48, -0.36]
-tcp_pos_11 = [0.245, -0.4043, 0.0959, 2.248, -2.19, 0.079] # tcp2 [-0.48, -0.16, 0.35, 0.3, 0.68, -0.68]
-
-# tcp_pos_9 = tcp_pos_4 # tcp_pos_8
-# tcp_pos_10 = tcp_pos_3 # tcp_pos_9
-# tcp_pos_11 = tcp_pos_4 # tcp_pos_7
 
 # enter the configuration of the tcp points you want to use
 tcp_poses_sel = []
@@ -65,6 +65,6 @@ for pos in tcp_pos_config:
 
 tcp_poses_all = []
 # make a dynaminc list of tcp positions
-for i in range(12):
-    tcp_poses_all.append(globals()["tcp_pos_%i" % i])
+# for i in range(12):
+#     tcp_poses_all.append(globals()["tcp_pos_%i" % i])
 
